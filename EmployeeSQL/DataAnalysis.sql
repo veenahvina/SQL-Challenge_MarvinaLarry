0e0371049954ac877afbac406421ac20dc9f372b
marvina <anew9107life20xx@gmail.com>
2023-08-27 04:08:38 -0500

    DataAnalysis2

--- EmployeeSQL/DataAnalysis.sql
+++ EmployeeSQL/DataAnalysis.sql
@@ -16,3 +16,25 @@ SELECT dm.emp_no, dm.dept_no, d.dept_name, e.last_name, e.first_name
 FROM dept_manager As dm
 JOIN employees As e ON e.emp_no = dm.emp_no
 JOIN departments As d ON dm.dept_no = d.dept_no
+
+--List the department number for each employee along with that employee’s employee number, last name, first name, and department name
+
+SELECT d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+FROM departments As d
+JOIN dept_emp As de ON de.dept_no = d.dept_no
+JOIN employees As e ON de.emp_no = e.emp_no
+
+--List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
+
+SELECT e.first_name, e.last_name, e.sex
+FROM employees As e
+WHERE e.first_name = 'Hercules' AND e.last_name LIKE 'B%';
+
+--List each employee in the Sales department, including their employee number, last name, and first name
+
+SELECT d.dept_name, e.emp_no, e.last_name, e.first_name
+FROM departments As d
+JOIN dept_emp As de ON de.dept_no = d.dept_no
+JOIN employees As e ON de.emp_no = e.emp_no
+WHERE d.dept_name = 'Sales'
+
